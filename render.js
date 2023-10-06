4c22c10dea25587dfa0fd713fe47ec03b00ab0d5
maksrover <maksrover@gmail.com>
2023-10-06 15:57:03 +0300

    working on additional

--- render.js
+++ render.js
@@ -1,5 +1,6 @@
 import { appComment, appFormElement  } from "./ui.js";
 import { initLikeComments, initReplyComment, deleteComment } from "./renderComments.js";
+import { formatSwedishDate } from "./lib/formatDate/formatDate.js";
 
 
 
@@ -7,30 +8,36 @@ import { initLikeComments, initReplyComment, deleteComment } from "./renderComme
 export { renderComments, renderForms };
 
 
-    function renderComments({ comments }) {  
-      const commentsHTML = comments
-          .map((comment, ind) => {
-          let currentTime = new Date(comment.date);
-          let commentTime = `${currentTime.toLocaleDateString('ru-Ru', { day: "2-digit", month: "2-digit", year: "2-digit" })} ${currentTime.toLocaleTimeString('ru-Ru', { hour: "2-digit", minute: "2-digit" })}`;
-          return `
-              <li data-index="${comment.id}" class="comment">
-                  <div class="comment-header">
-                      <div>${comment.author.name} (${comment.author.login})</div>
-                      <div>${commentTime}</div>
+function renderComments({ comments }) {
+  const commentsHTML = comments
+    .map((comment, ind) => {
+      const currentTime = new Date(comment.date);
+      const commentTime = formatSwedishDate(currentTime); 
+      return `
+          <li data-index="${comment.id}" class="comment">
+              <div class="comment-header">
+                  <div>${comment.author.name} (${comment.author.login})</div>
+                  <div>${commentTime}</div>
+              </div>
+              <div class="comment-body">
+                  <div class="comment-text">
+                  ${comment.text.replaceAll("QUOTE_BEGIN", "<div class='quote'>").replaceAll("QUOTE_END", "</div>")}
                   </div>
-                  <div class="comment-body">
-                      <div class="comment-text">
-                      ${comment.text.replaceAll("QUOTE_BEGIN", "<div class='quote'>").replaceAll("QUOTE_END", "</div>")}
-                      </div>
+              </div>
+              <div class="comment-footer">
+                  <div class="likes">
+                  <span class="likes-counter">${comment.likes}</span>
+                  <button data-index="${ind}" class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
                   </div>
-                  <div class="comment-footer">
-                      <div class="likes">
-                      <span class="likes-counter">${comment.likes}</span>
-                      <button data-index="${ind}" class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
-                      </div>
-                  </div>
-              </li> `;
-        }).join("");
+              </div>
+          </li> `;
+    })
+    .join("");
+
+  appComment.innerHTML = `<ul class="comments">${commentsHTML}</ul>
+    <div class="delete-form">
+    <button class="delete-form-button">Удалить последний комментарий</button>
+    </div>`;
 
     
       appComment.innerHTML = `<ul class="comments">${commentsHTML}</ul>
